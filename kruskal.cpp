cf63db67f233bb97d2bc266079297caf1dc405ad
shivam17u113 <46968180+shivam17u113@users.noreply.github.com>
2019-04-29 20:18:12 +0530

    Update kruskal.cpp
    MODIFIED

--- kruskal.cpp
+++ kruskal.cpp
@@ -1,175 +1,151 @@
-#include <iostream>
-#include<bits/stdc++.h>
-using namespace std;
+//Kruskal's Algorithm	 
+	#include <iostream>
+	using namespace std;
 
-
-int main()
- {
- 	int n_v,n_e,v1,v2,value,k=0;
-	cout<<"\n enter the no of vertices  ";
-	cin>>n_v;
-	cout<<"\n enter the no of edges ";
-	cin>>n_e;
-	
-	int g[n_v][n_v];
-	int distance[n_v],visited[n_v],parent[n_v];
-	
-	int sort_arr[n_v];
-	
-	for(int i=0;i<n_v;i++)
-	{
-		distance[i]=0;
-		visited[i]=0;
-	//	parent[i]=0;
-		sort_arr[i]=0;
-	}
-	
-	distance[0]=0;
-	
-	
-	for(int i=0;i<n_v;i++)
-	{
-		for(int j=0;j<n_v;j++)
-	  {
-	  	g[i][j]=0;
-	  }
-	     
-	}
-	      
-	         
-	
-		for(int j=0;j<n_e;j++)
-		{
-			cout<<"enter the starting vertex, ending vertex and the value ";
-			cin>>v1;
-			cin>>v2;
-			cin>>value;
-			
-			g[v1][v2]=value;
-		}
-		
-		cout<<"the given mateix is";
-		for(int i=0;i<n_v;i++)
-	{ 
-	   cout<<endl;
-		for(int j=0;j<n_v;j++)
-	  {
-	  	cout<<g[i][j];
-	  	cout<<"  ";
-	  }
-	     
-	}
-		cout<<endl;
-		
- 	 k=0;
- 	for(int i=0; i<n_v;i++)
-	 {
-	 	for(int j=0;j<n_v;j++)
-	 	{
-	 		if(g[i][j]!=0)
-                            {
-                 	sort_arr[k]=g[i][j];
-                 	cout<<sort_arr[k]<<endl;
-                 	  k++;
-                 	
-                 	
-							  }	 		
-		   }
-	 	
-		 }	
- 	
- 	cout<<"\n \n before sort";
- 	for(int i=0;i<k;i++)
- 	{
- 		cout<<sort_arr[i];
- 		cout<<endl;
- 		
-	 }
- 	
- 
- for(int i=0;i<k;i++)
- {
-  for(int j=0;j<k;j++)
-  {
-  	
-  	if(sort_arr[i]<sort_arr[j])
-  	   {
-  	   	int temp;
-  	   	temp=sort_arr[i];
-  	   	sort_arr[i]=sort_arr[j];
-  	   	sort_arr[j]=temp;
-			}   
-  	
- 	
- }
- }
- 
- 
- 	cout<<"\n \n after sort";
- 	for(int i=0;i<k;i++)
- 	{
- 		cout<<sort_arr[i];
- 		cout<<endl;
- 		
-	 }
- 	
- 	int m=0;
-for(int i=0;i<n_v;i++)
- 	{
- 		
- 		for(int j=0;j<n_v;j++)
- 		{
- 			
- 			if(g[i][j]==sort_arr[m])
-                  {
-              	if(visited[j]==0)
-              	{
-              		distance[m]=sort_arr[m];
-              		m++;
-              		visited[j]=1;
-              		cout<<" the distance matrix "<<distance[m]<<endl;
-              		
-              		
-				  }
-              	
-						   } 			
- 			
-		 }
- 		
- 		
- 		
- 		
-	 }
-	 cout<<" the distancr"<<endl;
-	 for(int i=0;i<k;i++)
- 	{
- 		cout<<distance[i];
- 		
- 		
-	 }
+struct Edge {
 	 
- 	
- 	int sum=0;
- 	for(int i=0;i<m;i++)
- 	{
- 		sum=distance[i]+sum;
- 		
- 		
-	}
- 	cout<<"the minimum ditance is "<<sum;
- 	
- 	return 0;
- }
- 
- 
- 
- 
- 
- 
- 
- 
- 	
- 	
- 	
- 	
+	int u, v, wt;
+	 
+};
+
+	int n, m, mincost, i, j;
+	int checkCycle (Edge, int*);
+	  
+
+
+
+int main() 
+{
+  
+	cout << "Enter the number of vertices in the graph: ";
+	cin >> n;
+	 
+	cout << "Enter the number of edges in the graph: ";
+	cin >> m;
+	 
+	int path[50];
+	 
+	struct Edge e[50];
+	 
+	struct Edge t;
 	
+ 
+	for (i=0; i<m; i++) 
+	{
+	 
+	cout << "Enter 2 vertices and weight of edge " << i+1 << endl;
+	 
+	cout << "First vertex: ";
+	 
+	cin >> e[i].u;
+	 
+	cout << "Second vertex: ";
+	 
+	cin >> e[i].v;
+	 
+	cout << "Weight: ";
+ 
+	cin >> e[i].wt;
+	 
+	//cout << endl
+	 
+	}
+	 
+	// Sorting the edges in ascending order of weights
+	 
+	for (i=0; i<=m-1; i++) 
+	{
+	for (j=0; j<m-i-1; j++) 
+	{
+	if (e[j].wt > e[j+1].wt)
+	{
+	 
+		t = e[j];
+	 	e[j] = e[j+1];
+	 	e[j+1] = t;
+ 
+	}
+	}
+	}
+	 
+
+	 
+	for (i=1; i<=n; i++) 
+	{
+	 
+	path[i] = 0;
+	 
+	}
+	 
+	// Counts the number of edges selected in the tree
+	i = 0;
+	 
+	// Counts the number of edges selected or discarded
+	j = 0;
+	mincost = 0;
+	 
+	//cout << endl;
+ 
+
+
+while ((i!=n-1) && (j!=m)) 
+	{
+	 
+	cout << "Edge ("<< e[j].u << ", " << e[j].v << ") "<< "with weight " << e[j].wt << " ";
+	 
+	if (checkCycle(e[j], path)) 
+	{
+	
+	mincost = mincost + e[j].wt;
+	i++;
+	cout << "is selected";
+ 
+	} else {
+ 
+	cout << "is discarded";
+	 
+	}
+	 
+	cout << endl;
+ 
+	j++;
+	 
+	}
+ 
+	if (i!=n-1) {
+	 
+	cout << "Minimum spanning tree cannot be formed ";
+	 
+	}
+
+ 	cout<<"Minimun Cost is "<<mincost;
+	return 0;
+	 
+	}
+	 
+
+
+
+int checkCycle (Edge e, int* path)
+{
+	 
+	int u = e.u, v = e.v;
+	 
+	while (path[u] > 0)
+	u = path[u];
+	 
+	while (path[v] > 0)
+	v = path[v];
+	 
+	if (u != v)
+	{
+	 
+	 path[u] = v;
+	 
+	 return 1;
+	 
+	}
+	 
+	return 0;
+	 
+}
